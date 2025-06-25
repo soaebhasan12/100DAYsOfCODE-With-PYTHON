ce03608343cb2f643a3e0bf9a714b744cceb7a60
ShoaibHassan11 <saibuhasan22@gmail.com>
2025-06-25 05:47:40 +0530

    HiGHER LOWER GAME PROJECT

---
+++ Day-14 HiGHER LOWER GAME PROJECT/game_data.py
@@ -0,0 +1,332 @@
+data = [
+    {
+        'name': 'Instagram', 
+        'follower_count': 346, 
+        'description': 'Social media platform', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Virat Kohli', 
+        'follower_count': 255, 
+        'description': 'Cricketer', 
+        'country': 'India'
+    },
+    {
+        'name': 'Cristiano Ronaldo', 
+        'follower_count': 215, 
+        'description': 'Footballer', 
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Amitabh Bachchan', 
+        'follower_count': 200, 
+        'description': 'Actor', 
+        'country': 'India'
+    },
+    {
+        'name': 'Ariana Grande', 
+        'follower_count': 183, 
+        'description': 'Musician and actress', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Shah Rukh Khan', 
+        'follower_count': 190, 
+        'description': 'Actor', 
+        'country': 'India'
+    },
+    {
+        'name': 'Dwayne Johnson', 
+        'follower_count': 181, 
+        'description': 'Actor and professional wrestler', 
+        'country': 'United States'
+    },
+    {
+        'name': 'MS Dhoni', 
+        'follower_count': 175, 
+        'description': 'Cricketer', 
+        'country': 'India'
+    },
+    {
+        'name': 'Selena Gomez', 
+        'follower_count': 174, 
+        'description': 'Musician and actress', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Sachin Tendulkar', 
+        'follower_count': 160, 
+        'description': 'Cricket Legend', 
+        'country': 'India'
+    },
+    {
+        'name': 'Kylie Jenner', 
+        'follower_count': 172, 
+        'description': 'Reality TV personality and businesswoman', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Deepika Padukone', 
+        'follower_count': 150, 
+        'description': 'Actress', 
+        'country': 'India'
+    },
+    {
+        'name': 'Kim Kardashian', 
+        'follower_count': 167, 
+        'description': 'Reality TV personality and businesswoman', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Ranveer Singh', 
+        'follower_count': 148, 
+        'description': 'Actor', 
+        'country': 'India'
+    },
+    {
+        'name': 'Lionel Messi', 
+        'follower_count': 149, 
+        'description': 'Footballer', 
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Alia Bhatt', 
+        'follower_count': 140, 
+        'description': 'Actress', 
+        'country': 'India'
+    },
+    {
+        'name': 'Beyoncé', 
+        'follower_count': 145, 
+        'description': 'Musician', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Akshay Kumar', 
+        'follower_count': 138, 
+        'description': 'Actor', 
+        'country': 'India'
+    },
+    {
+        'name': 'Neymar', 
+        'follower_count': 138, 
+        'description': 'Footballer', 
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic', 
+        'follower_count': 135, 
+        'description': 'Magazine', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Hrithik Roshan', 
+        'follower_count': 130, 
+        'description': 'Actor', 
+        'country': 'India'
+    },
+    {
+        'name': 'Justin Bieber', 
+        'follower_count': 133, 
+        'description': 'Musician', 
+        'country': 'Canada'
+    },
+    {
+        'name': 'Katrina Kaif', 
+        'follower_count': 128, 
+        'description': 'Actress', 
+        'country': 'India'
+    },
+    {
+        'name': 'Taylor Swift', 
+        'follower_count': 131, 
+        'description': 'Musician', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Mahindra Group', 
+        'follower_count': 125, 
+        'description': 'Conglomerate', 
+        'country': 'India'
+    },
+    {
+        'name': 'Jennifer Lopez', 
+        'follower_count': 119, 
+        'description': 'Musician and actress', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Ratan Tata', 
+        'follower_count': 115, 
+        'description': 'Industrialist & Philanthropist', 
+        'country': 'India'
+    },
+    {
+        'name': 'Nicki Minaj', 
+        'follower_count': 113, 
+        'description': 'Musician', 
+        'country': 'Trinidad and Tobago'
+    },
+    {
+        'name': 'Reliance Industries', 
+        'follower_count': 110, 
+        'description': 'Business Conglomerate', 
+        'country': 'India'
+    },
+    {
+        'name': 'Nike', 
+        'follower_count': 109, 
+        'description': 'Sportswear multinational', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Amul', 
+        'follower_count': 84, 
+        'description': 'Dairy Brand', 
+        'country': 'India'
+    },
+    {
+        'name': 'FC Barcelona', 
+        'follower_count': 85, 
+        'description': 'Football club', 
+        'country': 'Spain'
+    },
+    {
+        'name': 'Demi Lovato', 
+        'follower_count': 80, 
+        'description': 'Musician and actress', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Tata Motors', 
+        'follower_count': 75, 
+        'description': 'Automobile Manufacturer', 
+        'country': 'India'
+    },
+    {
+        'name': 'Drake', 
+        'follower_count': 65, 
+        'description': 'Musician', 
+        'country': 'Canada'
+    },
+    {
+        'name': 'Flipkart', 
+        'follower_count': 62, 
+        'description': 'E-commerce Platform', 
+        'country': 'India'
+    },
+    {
+        'name': 'LeBron James', 
+        'follower_count': 63, 
+        'description': 'Basketball player', 
+        'country': 'United States'
+    },
+    {
+        'name': 'Narendra Modi', 
+        'follower_count': 50, 
+        'description': 'Prime Minister of India', 
+        'country': 'India'
+    },
+    {
+        'name': 'Mukesh Ambani', 
+        'follower_count': 45, 
+        'description': 'Business Magnate', 
+        'country': 'India'
+    },
+    {
+        'name': 'Sundar Pichai', 
+        'follower_count': 40, 
+        'description': 'CEO of Google', 
+        'country': 'India'
+    },
+    {
+        'name': 'ISRO', 
+        'follower_count': 35, 
+        'description': 'Indian Space Research Organisation', 
+        'country': 'India'
+    },
+    {
+        'name': 'Indian Army', 
+        'follower_count': 30, 
+        'description': 'Defense Force', 
+        'country': 'India'
+    },
+    {
+        'name': 'T-Series', 
+        'follower_count': 200, 
+        'description': 'Music Label', 
+        'country': 'India'
+    },
+    {
+        'name': 'Salman Khan', 
+        'follower_count': 120, 
+        'description': 'Actor', 
+        'country': 'India'
+    },
+    {
+        'name': 'Rohit Sharma', 
+        'follower_count': 90, 
+        'description': 'Cricketer', 
+        'country': 'India'
+    },
+    {
+        'name': 'Anushka Sharma', 
+        'follower_count': 95, 
+        'description': 'Actress', 
+        'country': 'India'
+    },
+    {
+        'name': 'Sania Mirza', 
+        'follower_count': 50, 
+        'description': 'Tennis Player', 
+        'country': 'India'
+    },
+    {
+        'name': 'PV Sindhu', 
+        'follower_count': 45, 
+        'description': 'Badminton Player', 
+        'country': 'India'
+    },
+    {
+        'name': 'Yuvraj Singh', 
+        'follower_count': 55, 
+        'description': 'Cricketer', 
+        'country': 'India'
+    },
+    {
+        'name': 'Sourav Ganguly', 
+        'follower_count': 60, 
+        'description': 'Cricket Administrator', 
+        'country': 'India'
+    },
+    {
+        'name': 'Jio', 
+        'follower_count': 100, 
+        'description': 'Telecom Company', 
+        'country': 'India'
+    },
+    {
+        'name': 'Zomato', 
+        'follower_count': 50, 
+        'description': 'Food Delivery', 
+        'country': 'India'
+    },
+    {
+        'name': 'Swiggy', 
+        'follower_count': 45, 
+        'description': 'Food Delivery', 
+        'country': 'India'
+    },
+    {
+        'name': 'Vistara', 
+        'follower_count': 30, 
+        'description': 'Airline', 
+        'country': 'India'
+    },
+    {
+        'name': 'Indigo Airlines', 
+        'follower_count': 35, 
+        'description': 'Airline', 
+        'country': 'India'
+    }
+]
