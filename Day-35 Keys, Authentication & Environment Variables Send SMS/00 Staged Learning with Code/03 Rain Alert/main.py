4eb6fe12b73b685accee0e6443f137102cb3fb75
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-07-07 11:47:27 +0530

    Removed Secrets key

--- Day-35 Keys, Authentication & Environment Variables Send SMS/00 Staged Learning with Code/03 Rain Alert/main.py
+++ Day-35 Keys, Authentication & Environment Variables Send SMS/00 Staged Learning with Code/03 Rain Alert/main.py
@@ -1,14 +1,14 @@
 import requests
 import json
 
-api_key = "6c53fd9deaa27688ad3a2ccfebf78263"                    # this a dummy key, replace with your own
+api_key = "asdfasdfasdfasdfasdfasdf"                    # this a dummy key, replace with your own
 # You can get your own API key from https://openweathermap.org/api/one-call-
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"            
 
 weather_params = {
-    "lat": 29.852970,
-    "lon": 77.875450,
+    "lat": 00.000000,
+    "lon": 00.000000,
     "appid": api_key,
 }
 
--- Day-35 Keys, Authentication & Environment Variables Send SMS/00 Staged Learning with Code/04 Rain Alert/main.py
+++ Day-35 Keys, Authentication & Environment Variables Send SMS/00 Staged Learning with Code/04 Rain Alert/main.py
@@ -11,14 +11,14 @@
 import requests
 import json
 
-api_key = "6c53fd9deaa27688ad3a2ccfebf78263"                    # this a dummy key, replace with your own
+api_key = "000000000000000000000"                    # this a dummy key, replace with your own
 # You can get your own API key from https://openweathermap.org/api/one-call-
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"            
 
 weather_params = {
-    "lat": 29.852970,
-    "lon": 77.875450,
+    "lat": 000000000,
+    "lon": 000000000,
     "appid": api_key,
     "exclude": "current,minutely,daily"
 }
